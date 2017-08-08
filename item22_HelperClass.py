de8258e7b9e0ed72d7564db20f9f9da254d40bfb
山田宏樹 <koki@yamadahirokinoMacBook-Pro.local>
2017-08-08 23:29:32 +0900

    add class WeightGradebook

--- item22_HelperClass.py
+++ item22_HelperClass.py
@@ -35,3 +35,36 @@ class BySubjectGradebook():
             total += sum(grades)
             count += len(grades)
         return total / count
+
+
+class WeightGradebook():
+    def __init__(self):
+        self._grades = {}
+
+    def add_student(self, name):
+        self._grades[name] = {}
+
+    def report_grade(self, name, subject, score, weight):
+        by_subject = self._grades[name]
+        grade_list = by_subject.setdefault(subject, [])
+        grade_list.append((score, weight))
+
+    def average_grade(self, name):
+        by_subject = self._grades[name]
+        score_sum, score_count = 0, 0
+        for subject, scores in by_subject.items():
+            subject_avg, total_weight = 0, 0
+            for score, weight in scores:
+                subject_avg += score * weight
+                total_weight += weight
+            score_sum += subject_avg / total_weight
+            score_count += 1
+        return score_sum / score_count
+
+
+book = WeightGradebook()
+book.add_student('Albert Einstein')
+book.report_grade('Albert Einstein', 'Math', 80, 0.1)
+book.report_grade('Albert Einstein', 'Math', 90, 0.4)
+book.report_grade('Albert Einstein', 'Math', 70, 0.5)
+print(book.average_grade('Albert Einstein'))
