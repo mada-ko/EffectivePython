1b75a909b0358a70f30844404ddb7bf3d138e43d
aa369876 <aa369876@aa369876noMacBook-Air.local>
2017-08-10 14:19:04 +0900

    Complete item22

--- item22_HelperClass.py
+++ item22_HelperClass.py
@@ -68,3 +68,58 @@ book.report_grade('Albert Einstein', 'Math', 80, 0.1)
 book.report_grade('Albert Einstein', 'Math', 90, 0.4)
 book.report_grade('Albert Einstein', 'Math', 70, 0.5)
 print(book.average_grade('Albert Einstein'))
+
+# Refactoring
+
+import collections
+Grade = collections.namedtuple('Grade', ('score', 'weight'))
+
+
+class Subject():
+    def __init__(self):
+        self._grades = []
+
+    def report_grade(self, score, weight):
+        self._grades.append(Grade(score, weight))
+
+    def average_grade(self):
+        total, total_weight = 0, 0
+        for grade in self._grades:
+            total += grade.score * grade.weight
+            total_weight += grade.weight
+        return total / total_weight
+
+
+class Student():
+    def __init__(self):
+        self._subjects = {}
+
+    def subject(self, name):
+        if name not in self._subjects:
+            self._subjects[name] = Subject()
+        return self._subjects[name]
+
+    def average_grade(self):
+        total, count = 0, 0
+        for subject in self._subjects.values():
+            total += subject.average_grade()
+            count += 1
+        return total / count
+
+
+class Gradebook():
+    def __init__(self):
+        self._students = {}
+
+    def student(self, name):
+        if name not in self._students:
+            self._students[name] = Student()
+        return self._students[name]
+
+
+book = Gradebook()
+albert = book.student('Albert Einstein')
+math = albert.subject('Math')
+math.report_grade(80, 0.10)
+
+print(albert.average_grade())
